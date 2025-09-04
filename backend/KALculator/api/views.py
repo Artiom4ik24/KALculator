84bd29e512c692988039a02cb9f99103c1dfeddc
nadezdarom <romanenko.nadezda.04@gmail.com>
2025-09-04 16:56:09 +0300

    added history model

--- backend/KALculator/api/views.py
+++ backend/KALculator/api/views.py
@@ -2,6 +2,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .models import History
+from .serializers import HistorySerializer
 
 
 # Заглушка, которая всегда выдаёт число 732 и статус 200
@@ -17,15 +22,10 @@ def calculate(request):
 # Заглушка для эндпоинта с историей
 @api_view(["GET"])
 def history(request):
-    dummy = [
-        {
-            "equation": "2+2",
-            "answer": "4"
-        },
-        {
-            "equation": "5/0",
-            "answer": "ERR"
-        }
-    ]
-
-    return Response(dummy, status=status.HTTP_200_OK)
+    try:
+        history_list = History.objects.all()
+        serializer = HistorySerializer(history_list, many=True)
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except:
+        return Response({"message": "Database is unavailable"}, status=status.HTTP_400_BAD_REQUEST)
