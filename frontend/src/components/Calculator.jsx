5ae69a537bd1b48b0220a2d638e6906c108ff199
yuyuriff <normierandomzz@gmail.com>
2025-09-04 23:42:57 +0300

    connected frontend to calculate/ endpoint

--- frontend/src/components/Calculator.jsx
+++ frontend/src/components/Calculator.jsx
@@ -13,10 +13,45 @@ const Wrapper = styled.div`
   font-family: -apple-system, BlinkMacSystemFont, "San Francisco", "Helvetica Neue", sans-serif;
 `;
 
+const calculateOnServer = async (expression) => {
+  try {
+    const response = await fetch('http://127.0.0.1:8000/calculate/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ equation: expression })
+    });
+
+    if (response.ok) {
+      const answer = await response.text();
+      return answer;
+    } else {
+      const errorResponse = await response.json();
+      return errorResponse.message;
+    }
+
+  } catch (error) {
+    return "Server error.";
+  }
+};
+
 export default () => {
   const [display, setDisplay] = useState('');
 
-  const handleButton = (value, type) => {
+  const handleButton =  async (value, type) => {
+    if (type === 'send_string') {
+      if (!display)
+        return;
+      try {
+        const result = await calculateOnServer(display);
+        setDisplay(String(result));
+      } catch {
+        setDisplay('Error');
+        setTimeout(() => setDisplay(''), 1000)
+      }
+    }
+
     if (type === 'digit' || type === 'operation') {
       setDisplay(prev => prev + value);
     } else if (type === 'clear') {
